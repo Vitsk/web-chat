7d4e96a65fa92b379b61941ee71e7abcaba97db2
Vitsk <vitya23052@gmail.com>
2021-03-25 17:04:25 +0200

    fixing styles for icon

--- src/components/Main/Main/Messages/Message.tsx
+++ src/components/Main/Main/Messages/Message.tsx
@@ -11,12 +11,14 @@ export const Message: React.FC<TProps> = (props): React.ReactElement => {
   return (
     <>
       <div className={styles.messageContainer}>
-        <div className={`${styles.message} ${props.own ? styles.ownMessage : styles.incomingMessage}`}>
-          <p>{props.text}</p>
+        <div className={`${styles.messageWrapper} ${props.own ? styles.ownMessage : styles.incomingMessage}`}>
+          <div className={`${styles.message} ${props.own ? styles.ownMessage : styles.incomingMessage}`}>
+            <p>{props.text}</p>
+          </div>
+          {props.own && <div className={styles.profileIcon}>
+            <img className={styles.profileIconImg} src={props.profileIcon} alt="" />
+          </div>}
         </div>
-        {props.own && <div className={styles.profileIcon}>
-          <img className={styles.profileIconImg} src={props.profileIcon} alt="" />
-        </div>}
       </div>
     </>
   )
